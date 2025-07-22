16e9270ee82b6c6c68ca35563c7e22ef6cf86ffe
Viacheslav Azarov <public@religiosa.ru>
2025-07-22 21:25:07 +0200

    initial commit

---
+++ lua/plugins/tree-view.lua
@@ -0,0 +1,14 @@
+-- tree-view
+-- https://github.com/nvim-tree/nvim-tree.lua
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {}
+  end,
+}
