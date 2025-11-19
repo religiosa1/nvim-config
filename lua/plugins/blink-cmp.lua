a832b6aa0aa455aa9567b44a97cf315b996aa4d9
Viacheslav Azarov <public@religiosa.ru>
2025-11-19 21:47:26 +0100

    Change autocompletion preset to be more vscode-like

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "Saghen/blink.cmp",
+    opts = {
+      keymap = { preset = "super-tab" },
+      -- maybe I want to disable ghost_text as well, research later:
+      -- https://cmp.saghen.dev/configuration/general.html
+    },
+  },
+}
