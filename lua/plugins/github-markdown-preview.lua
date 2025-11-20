b0c35adf940eb3798b3ca3fbf6f6966919a84b4a
Viacheslav Azarov <public@religiosa.ru>
2025-11-20 21:30:27 +0100

    Custom command for github markdown preview

--- lua/plugins/github-markdown-preview.lua
+++ lua/plugins/github-markdown-preview.lua
@@ -1,8 +1,10 @@
 -- github-flavored markdown previewer with mermaid support
+-- Requires Bun.
 return {
   {
     "wallpants/github-preview.nvim",
-    cmd = { "GithubPreviewToggle" },
+    -- default comand is "GithubPreviewToggle", but I'd never remember that
+    cmd = { "MarkdownPreview" },
     -- keys = { "<leader>mpt" },
     opts = {
       details_tags_open = true,
@@ -13,7 +15,8 @@ return {
     config = function(_, opts)
       local gpreview = require("github-preview")
       gpreview.setup(opts)
-      -- local fns = gpreview.fns
+      local fns = gpreview.fns
+      vim.api.nvim_create_user_command("MarkdownPreview", fns.toggle, {})
       -- vim.keymap.set("n", "<leader>mpt", fns.toggle)
       -- vim.keymap.set("n", "<leader>mps", fns.single_file_toggle)
       -- vim.keymap.set("n", "<leader>mpd", fns.details_tags_toggle)
