e28c23b2cfd4f065dac2d93da3247dd58ee3fa7c
Viacheslav Azarov <public@religiosa.ru>
2025-12-08 18:35:24 +0100

    disable markdown linting

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        -- Default linter is markdownlint-cli2, which sucks a bag of dicks
+        markdown = {},
+      },
+    },
+  },
+}
