fd454bd9e7912c4a32c5a56b39dce4054cb60be3
Viacheslav Azarov <public@religiosa.ru>
2025-12-24 13:19:30 +0100

    switch to my fork of github-codeowners

--- lua/plugins/gh-co.lua
+++ lua/plugins/gh-co.lua
@@ -1,7 +1,11 @@
 -- Github Codeowners plugin
 -- https://github.com/comatory/gh-co.nvim
 return {
-  "comatory/gh-co.nvim",
+  -- "comatory/gh-co.nvim",
+  "religiosa1/gh-co.nvim",
+  -- dir = "~/Projects/gh-co.nvim/",
+  -- enabled = false,
+  version = "*",
   config = function()
     vim.keymap.set("n", "<leader>go", ":GhCoWho<CR>", { desc = "CodeOwner" })
   end,
