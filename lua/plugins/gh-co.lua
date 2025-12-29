19e0bba1bab92fa7cbc31b99b5ceea99988fcde9
Viacheslav Azarov <public@religiosa.ru>
2025-12-29 19:35:02 +0100

    switch back to comatory/gh-co.nvim + missed lua snippets

--- lua/plugins/gh-co.lua
+++ lua/plugins/gh-co.lua
@@ -1,11 +1,7 @@
 -- Github Codeowners plugin
 -- https://github.com/comatory/gh-co.nvim
 return {
-  -- "comatory/gh-co.nvim",
-  "religiosa1/gh-co.nvim",
-  -- dir = "~/Projects/gh-co.nvim/",
-  -- enabled = false,
-  version = "*",
+  "comatory/gh-co.nvim",
   config = function()
     vim.keymap.set("n", "<leader>go", ":GhCoWho<CR>", { desc = "CodeOwner" })
   end,
