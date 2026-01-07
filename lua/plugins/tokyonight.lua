f80f8e108cb7c88d0b86135e80b416f825d68b86
Viacheslav Azarov <public@religiosa.ru>
2026-01-07 13:45:37 +0100

    tokyonight color scheme modified again

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,10 +3,18 @@ return {
   lazy = true,
   opts = {
     style = "moon",
-    on_colors = function(colors)
+    -- List of defaults: https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_moon.lua
+    on_highlights = function(colors)
       -- It's the color of unused args and functions, etc. I can't see it
-      -- on the background so we're making it brighter. Original was #444a73
-      colors.terminal_black = "#548ad3"
+      -- on the background so we're making it brighter. Original was #444a73 or colors.terminal_black
+      -- Overriding terminal_black color completely also messes up markdown_inline and ghost text for
+      -- autocompletion, so we're doing it in a more precise manner.
+      colors.DiagnosticUnnecessary = {
+        fg = "#548ad3",
+      }
     end,
+    -- on_colors = function(colors)
+    --   colors.terminal_black = "#548ad3"
+    -- end,
   },
 }
