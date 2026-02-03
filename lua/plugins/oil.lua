6796437a75d79f6b6cffca43c877f8a454d2a625
Viacheslav Azarov <public@religiosa.ru>
2026-02-03 19:56:05 +0100

    open oil as float on <leader>o

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,19 @@
 return {
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      float = {
+        -- Padding around the floating window
+        padding = 2,
+        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        max_width = 0.85,
+        max_height = 0.85,
+        -- see :help nvim_open_win
+        border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
+      },
+    },
     -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
@@ -13,5 +23,5 @@ return {
   {
     "benomahony/oil-git.nvim",
     dependencies = { "stevearc/oil.nvim" },
-  }
+  },
 }
