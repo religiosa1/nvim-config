e0f03e5e285e47673850270a1e0471afc6064925
Viacheslav Azarov <public@religiosa.ru>
2026-02-07 09:36:52 +0100

    reminder about calculator regitser

--- lua/plugins/calcium.lua
+++ lua/plugins/calcium.lua
@@ -1,4 +1,6 @@
--- Calcium for performing math calculations
+-- Calcium plugin for performing math calculations on the v-selected text
+-- Don't also forget about the builtin expression register aka "calculator":
+-- in insert mode: ctrl+R, =
 return {
   "necrom4/calcium.nvim",
   cmd = { "Calcium" },
