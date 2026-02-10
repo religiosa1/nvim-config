4773b031acbc5b872e5b27ddae9279feed5294be
Viacheslav Azarov <viacheslav.azarov_ges@omio.com>
2026-02-10 13:30:53 +0100

    add <C-j> and <C-k> bindings to blink completion

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -28,6 +28,8 @@ return {
 
         ["<Up>"] = { "select_prev", "fallback" },
         ["<Down>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
       },
     },
   },
