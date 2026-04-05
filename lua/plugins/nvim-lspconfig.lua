1f633b40d8cfbd9e83f4c55f8a2044f7e9609e00
Viacheslav Azarov <public@religiosa.ru>
2026-04-05 18:54:10 +0200

    Remove obsidian and marskman in favor of markdown-oxide

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,50 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      marksman = {
+        enabled = false,
+      },
+      -- markdown-oxide
+      markdown_oxide = {
+        -- Ensure that dynamicRegistration is enabled
+        -- This allows the LS to take into account actions like Create Unresolved File, etc
+        capabilities = vim.tbl_deep_extend(
+          "force",
+          vim.lsp.protocol.make_client_capabilities(),
+          require("blink.cmp").get_lsp_capabilities(),
+          {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = true,
+              },
+            },
+          }
+        ),
+        on_attach = function(_client, bufnr)
+          local function codelens_supported(bufnr)
+            for _, c in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+              if c.server_capabilities and c.server_capabilities.codeLensProvider then
+                return true
+              end
+            end
+            return false
+          end
+
+          vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "CursorHold", "BufEnter" }, {
+            buffer = bufnr,
+            callback = function()
+              if codelens_supported(bufnr) then
+                vim.lsp.codelens.refresh({ bufnr = bufnr })
+              end
+            end,
+          })
+
+          if codelens_supported(bufnr) then
+            vim.lsp.codelens.refresh({ bufnr = bufnr })
+          end
+        end,
+      },
+    },
+  },
+}
--- lua/plugins/override_snacks.lua
+++ lua/plugins/override_snacks.lua
@@ -5,14 +5,6 @@ return {
     image = {
       -- Plugin is disabled by default, so no inline rending is there in markdown
       enabled = true,
-      resolve = function(path, src)
-        -- Obsidian md images resolving.
-        local api = require("obsidian.api")
-        local is_note_page = api.path_is_note(path)
-        if is_note_page then
-          return api.resolve_attachment_path(src)
-        end
-      end,
     },
     picker = {
       win = {
@@ -27,6 +19,6 @@ return {
           },
         },
       },
-    }
+    },
   },
 }
