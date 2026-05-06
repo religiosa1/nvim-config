a32c41704cced4702074b3b4edfdd9c64015795b
Viacheslav Azarov <viacheslav.azarov_ges@omio.com>
2026-05-06 15:09:10 +0200

    bring back tabout

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,7 +1,7 @@
 return {
   {
     "abecodes/tabout.nvim",
-    disable = true,
+    disabled = false,
     lazy = false,
     config = function()
       require("tabout").setup({
@@ -20,6 +20,7 @@ return {
           { open = "(", close = ")" },
           { open = "[", close = "]" },
           { open = "{", close = "}" },
+          { open = "<", close = ">" },
         },
         ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
         exclude = {}, -- tabout will ignore these filetypes
