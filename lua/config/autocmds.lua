f7abcf3271401ec81710cbbf41e9ae400b74d454
Viacheslav Azarov <public@religiosa.ru>
2026-05-20 23:31:29 +0200

    syncing grug-far on write

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -27,3 +27,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.lsp.document_color.enable(true, nil, { style = "virtual" })
   end,
 })
+
+-- syncing grug-far on write
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "grug-far" },
+  callback = function(args)
+    local buf_id = args.buf
+
+    -- we need to schedule, as grug-far sets the option to none on buf setup
+    vim.schedule(function()
+      vim.bo[buf_id].buftype = "acwrite"
+      -- also need a name for the buffer, or :w will complain
+      if vim.api.nvim_buf_get_name(buf_id) == "" then
+        vim.api.nvim_buf_set_name(buf_id, "grug-far://" .. buf_id)
+      end
+
+      vim.api.nvim_create_autocmd("BufWriteCmd", {
+        buffer = buf_id,
+        callback = function()
+          require("grug-far").get_instance(0):sync_all()
+          vim.bo[buf_id].modified = false
+        end,
+      })
+    end)
+  end,
+})
