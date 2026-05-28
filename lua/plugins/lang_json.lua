6463bd0161e9ee1c2d19ade3abb15b60ecfcf5fe
Viacheslav Azarov <viacheslav.azarov_ges@omio.com>
2026-05-28 11:00:29 +0200

    copy json path plugin

---
+++ lua/plugins/lang_json.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "phelipetls/jsonpath.nvim",
+    ft = { "json", "jsonc" },
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "json", "jsonc" },
+        callback = function()
+          vim.keymap.set("n", "<leader>jp", function()
+            local jp = require("jsonpath").get()
+            vim.fn.setreg("+", jp)
+            vim.notify(jp, vim.log.levels.INFO, { title = "Copied JSON path" })
+          end, { buffer = true, desc = "Copy JSON path" })
+        end,
+      })
+    end,
+  },
+}
