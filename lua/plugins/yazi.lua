84542133ba4ee56628842deb192cde02a68a2f51
Viacheslav Azarov <public@religiosa.ru>
2026-06-28 14:50:23 +0200

    disable open yazi at the cwd

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -14,12 +14,12 @@ return {
         "<cmd>Yazi<cr>",
         desc = "Open yazi at the current file",
       },
-      {
-        -- Open in the current working directory
-        "<leader>cw",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
+      -- {
+      --   -- Open in the current working directory
+      --   "<leader>cw",
+      --   "<cmd>Yazi cwd<cr>",
+      --   desc = "Open the file manager in nvim's working directory",
+      -- },
       -- {
       --   "<c-a-up>", -- default is Ctrl-Up, but it conflicts with window resize
       --   "<cmd>Yazi toggle<cr>",
