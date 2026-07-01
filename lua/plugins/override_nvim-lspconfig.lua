2b74d39454b4ba6e36268354ce5ee9f16f210bf1
Viacheslav Azarov <public@religiosa.ru>
2026-07-01 22:20:11 +0200

    Cyrillic <leader>ca for codebook spelling fixes

--- lua/plugins/override_nvim-lspconfig.lua
+++ lua/plugins/override_nvim-lspconfig.lua
@@ -3,5 +3,19 @@ return {
   opts = {
     -- disabling inlay hints by default, still can be toggled with <leader>uh
     inlay_hints = { enabled = false },
+    servers = {
+      ["*"] = {
+        keys = {
+          -- Cyrillic (ЙЦУКЕН) duplicate of <leader>ca Code Action: с=c, ф=a
+          {
+            "<leader>сф",
+            vim.lsp.buf.code_action,
+            desc = "Code Action",
+            mode = { "n", "x" },
+            has = "codeAction",
+          },
+        },
+      },
+    },
   },
 }
